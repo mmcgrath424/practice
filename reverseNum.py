59c925eb86f1c0372133bc72da096bfdb53db84a
mmcgrath424 <mmcgrath424@gmail.com>
2018-05-08 17:44:12 -0400

    # reverse an integer

---
+++ reverseNum.py
@@ -0,0 +1,21 @@
+import os
+
+def reverseNum(i_num):
+	o_num = 0
+	neg_check = 1
+	if i_num < 0:
+		neg_check = -1
+	
+	i_num = i_num * neg_check	
+	
+	while i_num > 0:
+		o_num = o_num * 10
+		o_num = o_num + (i_num % 10)
+		i_num = i_num / 10
+		
+	o_num = o_num * neg_check
+	return o_num
+
+print reverseNum(-178564)
+
+
