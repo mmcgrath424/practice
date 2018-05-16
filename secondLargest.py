25e41c4adabde07c04b1f5d6ec34e5ff7affedb8
mmcgrath424 <mmcgrath424@gmail.com>
2018-05-15 22:27:46 -0400

    cleaning up code

--- secondLargest.py
+++ secondLargest.py
@@ -1,27 +1,41 @@
 import os
 
 data_set = [108,7,50,22,37,108,88,53,34,44,92,107,67,-3,-7,97]
-n = 10
-n_largest_offsets = [0]
+#data_set = [108,7,50,22,37]
+n = 5
+n_largest_offsets = []
 largest = 0
 second_largest = 0
+counter = 0
 
 
 for x in range (0,len(data_set)):
 	if data_set[x]>=data_set[largest]:
-		second_largest = largest
 		largest = x
-	elif data_set[x] > data_set[second_largest] or second_largest is 0:
+	if data_set[largest] is data_set[second_largest]:
+		second_largest = x
+	if (data_set[x] > data_set[second_largest] and
+	data_set[x] < data_set[largest] and   data_set[second_largest] < data_set[largest]):
 		second_largest = x
 		
-for y in range (0,len(data_set)):
-	for z in range (0,len(n_largest_offsets)):
-		if data_set[y] > data_set[n_largest_offsets[z]]:
-			n_largest_offsets.insert(z,y)
-	print data_set[y]
-	print data_set[n_largest_offsets[z]]
-	print n_largest_offsets  	
+for i in range (0,n):
+	n_largest_offsets.append(i)
 
+for y in range (0,len(data_set)):
+	counter = 0
+	while counter < n :
+		if data_set[y] != data_set[n_largest_offsets[counter]]:
+			if (data_set[y] > data_set[n_largest_offsets[counter]] and 
+			data_set[y] != data_set[n_largest_offsets[counter-1]]):
+				n_largest_offsets.insert(counter,y)
+				n_largest_offsets.pop()
+				counter = n # exit loop once a value is inserted
+		else :
+			counter = n  # exit loop if a dupe is found
+		counter = counter + 1
+
+		print n_largest_offsets  	
 
 print "Largest: " + str(data_set[largest]) + " Offset: " + str(largest)
-print "Second largest: " + str(data_set[second_largest]) + " Offset: " + str(second_largest)
+print ("Second largest: " + str(data_set[second_largest]) + " Offset: " + 
+str(second_largest))
