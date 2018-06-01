a2527273ae3291434545d45cbdfd489d578b126b
mmcgrath424 <mmcgrath424@gmail.com>
2018-05-31 22:55:47 -0400

    fixed some issues

--- LinkedList.py
+++ LinkedList.py
@@ -37,6 +37,7 @@ class LinkedList(object):
 		if self.num_nodes==0:
 			self.tail = ListNode()
 			self.head = self.tail
+			self.tail.set_value(node_val)
 		else:
 			temp_node = ListNode()
 			self.tail.set_next(temp_node)
@@ -50,6 +51,7 @@ class LinkedList(object):
 		if self.num_nodes==0:
 			self.head = ListNode()
 			self.tail = self.head
+			self.head.set_value(node_val)
 		else:
 			temp_node = ListNode()
 			self.head.set_prev(temp_node)
@@ -60,22 +62,20 @@ class LinkedList(object):
 		
 # Insert a node at the given node number		
 	def insert(self,node_num,node_val):
-		if self.num_nodes == 0:
-			print ("You can't insert into an empty list")
-			return None
-		new_node = ListNode()
-		new_node.set_value(node_val)
-		temp_node = self.get_node(node_num) # store node to adjust prev value
-		temp_node2 = temp_node.get_prev() # store prev node to adjust next value
-		new_node.set_prev(temp_node.get_prev())
-		new_node.set_next(temp_node)
-		temp_node.set_prev(new_node)
-		temp_node2.set_next(new_node)
 		if node_num == 0:
-			self.head = new_node
-		if node_num == self.num_nodes - 1:
-			self.tail = new_node
-		self.num_nodes += 1
+			self.prepend(node_val)
+		else:
+			new_node = ListNode()
+			new_node.set_value(node_val)
+			temp_node = self.get_node(node_num) # store node to adjust prev value
+			temp_node2 = temp_node.get_prev() # store prev node to adjust next value
+			new_node.set_prev(temp_node.get_prev())
+			new_node.set_next(temp_node)
+			temp_node.set_prev(new_node)
+			temp_node2.set_next(new_node)
+			if node_num == self.num_nodes - 1:
+				self.tail = new_node
+			self.num_nodes += 1
 
 # Remove the node number supplied		
 	def remove(self,node_num):
@@ -93,9 +93,9 @@ class LinkedList(object):
 			print ("out of range")
 			return 0
 		elif node_num == 0:
-			return self.head.get_value()
+			return self.head
 		elif node_num == self.num_nodes-1:
-			return self.tail.get_value()
+			return self.tail
 		else :
 			temp_node = self.head
 			for x in range (0,node_num):
@@ -113,6 +113,7 @@ class LinkedList(object):
 
 if __name__=='__main__':
 	test_list = LinkedList()
+	test_list.insert(0,100)
 	for x in range (1,10):
 		test_list.append(x)
 	
